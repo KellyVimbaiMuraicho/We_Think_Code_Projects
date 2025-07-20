f615b83ebac67f726e67a065368241d8382fac00
Kelly V Muraicho <muraichokelly@gmail.com>
2025-07-20 17:39:05 +0200

    einsten task

---
+++ einsten/einsten.py
@@ -0,0 +1,7 @@
+# input that prompts user for mass as an int in kgs.
+mass = int(input("m:"))
+#outputs the equivalent number of joules as an integer.
+joules = mass*( 300000000**2)
+
+print (f"E:{joules}")
+
